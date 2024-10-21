bb11d80a2e9bdcc976dd201d574ff7cc820285ed
gabormakraihmrc <gabormakraihmrc@users.noreply.github.com>
2024-10-21 14:23:59 +0100

    Review change

--- src/test/scala/uk/gov/hmrc/perftests/PhoneNumberVerificationRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/PhoneNumberVerificationRequests.scala
@@ -47,13 +47,13 @@ object PhoneNumberVerificationRequests extends ServicesConfiguration {
       .header("Content-Type", "application/json")
       .header("Accept", "application/json")
       .check(status.is(200))
-      .check(jsonPath("$.verificationCode").saveAs("passcode"))
+      .check(jsonPath("$.verificationCode").saveAs("verificationCode"))
   }
   
   val verifyPasscode: HttpRequestBuilder = {
     http("Verify a Passcode for the phone number")
       .post(s"$baseUrl$route/verify-code": String)
-      .body(StringBody(s"""{"phoneNumber" : "${phoneNumber}", "verificationCode": "$${passcode}" }"""))
+      .body(StringBody(s"""{"phoneNumber" : "${phoneNumber}", "verificationCode": "$${verificationCode}" }"""))
       .header("Content-Type", "application/json")
       .header("Accept", "application/json")
       .check(status.is(200))
